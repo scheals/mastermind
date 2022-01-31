54b7dbcfdaf57736053f27de95ec58d375f00d46
scheals <piecoo@gmail.com>
2022-01-31 19:56:14 +0100

    Introduce imperfect game loop Hints currently do not give the proper amount of 'exists' due to how array subtraction works.

--- mastermind.rb
+++ mastermind.rb
@@ -5,13 +5,80 @@ module Rules
   COLOURS = %w[pink red green blue purple yellow].freeze
   GUESS_SPACE = [' ', ' ', ' ', ' '].freeze
   HINT_SPACE = [' ', ' ', ' ', ' '].freeze
-  GAMEBOARD = [GUESS_SPACE, HINT_SPACE].freeze
+  GAMEBOARD = [[GUESS_SPACE], [HINT_SPACE]].freeze
 
-  def make_guess(guess, gameboard)
+  def make_guess(guess, turn)
+    return play unless legal?(guess)
 
-    p guess == player2.secret_code
-    p @turn += 1
-    Display.add_guess(guess, gameboard)
+    @turn += 1
+    win?(guess) ? codebreaker_wins : codemaker_wins(turn_limit?(turn))
+
+    display.add_guess(guess, turn)
+    return display.add_hint([' ', ' ', ' ', ' '], turn) unless check_any?(guess)
+
+    create_hint(guess, turn)
+    display.show_gameboard
+    play
+  end
+
+  def check_any?(guess)
+    return false if secret_code.intersection(guess).empty?
+
+    true
+  end
+
+  def create_hint(guess, turn)
+    hint = []
+    check_colours(check_matches(guess, hint), hint)
+
+    display.add_hint(hint, turn)
+  end
+
+  def check_colours(guess_without_perfect_matches, hint)
+    hint.push(guess_without_perfect_matches.intersection(secret_code).map { 'exists' })
+  end
+
+  def check_matches(guess, hint)
+    perfect_matches = guess.filter.with_index { |colour, i| colour == secret_code[i] }
+    hint.push(perfect_matches.map { 'perfect' })
+    guess - perfect_matches
+  end
+
+  def legal?(guess)
+    if guess.length != 4
+      puts 'There are four spaces to fill.'
+      return false
+    end
+    unless guess.all? { |colour| COLOURS.include?(colour) }
+      puts 'At least one of the colours is not a possible one in the game.'
+      return false
+    end
+    true
+  end
+
+  def win?(guess)
+    return true if guess == secret_code
+
+    false
+  end
+
+  def turn_limit?(turn)
+    p turn
+    return true if turn == 10
+
+    false
+  end
+
+  def codebreaker_wins
+    display.codebreaker_win
+    declare_winner
+  end
+
+  def codemaker_wins(turn_limit_reached)
+    if turn_limit_reached
+      display.codemaker_win
+      declare_winner
+    end
   end
 end
 
@@ -24,41 +91,71 @@ class Display
     @gameboard = [[GUESS_SPACE], [HINT_SPACE]]
   end
 
-  def self.introduce_rules
+  def introduce_rules
     puts 'These are the possible colours in the game:'
     print COLOURS
-    puts "\nWhen asked type the colours out in a particular order separated by a space."
+    puts "\nWhen asked type the colours out in a particular order separated by a space.\n\n"
     print GAMEBOARD
-    puts "\nThis is how the gameboard looks like - your guesses will be on the left and your hints will be on the right."
-    puts 'White on the right means there is such colour in code but not in its place.'
-    puts 'Black on the right means a colour is in its place.'
-    puts 'Good luck and have fun!'
+    puts "\nThis is how the gameboard looks like - your guesses will be on the left and your hints will be on the right.\n"
+    puts "'Exists' on the right means there is such colour in code but not in its place."
+    puts "'Perfect' on the right means a colour is in its place."
+    puts 'You\'ve got 10 turns to guess correctly.'
+    puts "\nGood luck and have fun!"
   end
 
-  def self.make_guess
-    puts 'What is your guess?'
+  def ask_guess
+    puts "\nWhat is your guess?"
   end
 
-  def self.add_guess(guess, gameboard)
-    gameboard[0][0] = guess if gameboard[0][0] == GUESS_SPACE
+  def add_guess(guess, turn)
+    return gameboard[0] = [guess] if turn == 1
+
+    gameboard[0].push(guess)
   end
 
-  def self.show_gameboard(gameboard)
-    print gameboard
+  def add_hint(hint, turn)
+    return gameboard[1] = [hint] if turn == 1
+
+    gameboard[1].push(hint)
+  end
+
+  def show_gameboard
+    gameboard[0].length.times do |i|
+      puts "#{gameboard[0][i]} #{gameboard[1][i]}"
+    end
+  end
+
+  def in_progress
+    puts 'Game is already in progress.'
+  end
+
+  def game_end
+    puts 'Game has already ended.'
+  end
+
+  def codebreaker_win
+    puts "Congratulations, you've guessed correctly!"
+  end
+
+  def codemaker_win
+    puts "Congratulations, your code was not broken!"
   end
 end
 
 # This class handles the Mastermind game.
 class Mastermind
   include Rules
-  attr_reader :player1_name, :player2_name, :game_name, :player1, :player2
+  attr_reader :player1_name, :player2_name, :game_name, :player1, :player2, :secret_code,
+              :display, :gameboard
 
   @game_count = 0
 
   def initialize
-    @gameboard = Display.new.gameboard
+    @display = Display.new
+    @gameboard = @display.gameboard
     @game_name = "Game #{self.class.count}"
-    @turn = 0
+    @turn = 1
+    @in_progress = true
   end
 
   def add_players(player1, player2)
@@ -71,15 +168,29 @@ class Mastermind
     "Added #{player1_name} and #{player2_name} to #{game_name}."
   end
 
-  def play
-    Display.introduce_rules
-    Display.make_guess
-    make_guess(gets.chomp.to_s.downcase.split(' '), @gameboard)
-    Display.show_gameboard(@gameboard)
+  def start
+    return display.in_progress unless @in_progress
+
+    display.introduce_rules
+    @secret_code = player2.create_code
+    p secret_code
+    play
   end
 
   def self.count
     @game_count += 1
+  end
+  private
+
+  def play
+    return display.game_end unless @in_progress
+
+    display.ask_guess
+    make_guess(gets.chomp.to_s.downcase.split(' '), @turn)
+  end
+
+  def declare_winner
+    @in_progress = false
   end
 end
 
@@ -107,7 +218,7 @@ end
 # This is a class that handles Computer players.
 class Computer < Player
   include Rules
-  attr_reader :name, :points, :ishuman, :secret_code
+  attr_reader :name, :points, :ishuman
 
   def initialize(name)
     super(name)
@@ -124,8 +235,5 @@ end
 space_oddysey = Mastermind.new
 brave = Human.new('Dave')
 hal = Computer.new('Hal')
-p brave
-p hal
-p hal.create_code
 space_oddysey.add_players(brave, hal)
-space_oddysey.play
+space_oddysey.start
